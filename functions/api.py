b40b9b0c78c446c4eec4249b0e23a5c1dc404469
Ismail ELyan <ismailalyan1234@gmail.com>
2025-06-02 14:10:47 +0300

    Delete functions directory

--- functions/api.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://badylk.netlify.app'},
-        'body': json.dumps({"message": "Welcome to the RecSys API.", "status": "running"})
-    }
